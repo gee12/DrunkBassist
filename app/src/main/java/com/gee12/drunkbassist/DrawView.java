77055778787f22829fa64cbb47a948a7f572cf97
gee12 <yagee12@gmail.com>
2015-02-17 18:37:13 +0500

    second commit

--- app/src/main/java/com/gee12/drunkbassist/DrawView.java
+++ app/src/main/java/com/gee12/drunkbassist/DrawView.java
@@ -67,6 +67,7 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
+    
     public void draw(Canvas canvas) {
         if (canvas == null) return;
 
